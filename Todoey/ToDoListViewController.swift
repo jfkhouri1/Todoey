6a880585f35e70a914e4e4429bc64ea743709343
Joseph F. Khouri <jfkhouri1@gmail.com>
2019-09-13 19:10:06 -0700

    Add new to do item functionality completed

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -4,13 +4,13 @@
 //
 //  Created by jkhouri on 9/13/19.
 //  Copyright © 2019 Joseph Khouri Consulting. All rights reserved.
-//
+//""
 
 import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    let itemArray = ["To do list 1", "To do list 2", "To do list 3"]
+    var itemArray = ["To do list 1", "To do list 2", "To do list 3"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,14 +18,57 @@ class ToDoListViewController: UITableViewController {
         
     }
 
-    //TODO: Declare cellForRowAtIndexPath here:
+    @IBAction func addTodoItemPressed(_ sender: Any) {
+        // add to do item on nav bar is pressed.
+        
+        var newItemTextField = UITextField()
+        
+        // create an alert dialog box
+        let alert = UIAlertController(title: "Add a New Todo Item", message: "", preferredStyle: .alert)
+        
+        // define an action to attach to this alert
+        let addAction = UIAlertAction(title: "Add Item", style: .default) { (alertAction) in
+            // what will happen once the user hits the action "Add Item" button on our alert
+            
+            // add new item to the list
+            print("Placeholder: \(newItemTextField.placeholder)")
+            print("Added Item: \(newItemTextField.text)")
+            
+            self.itemArray.append(newItemTextField.text ?? "")
+            self.tableView.reloadData()
+            
+            
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertAction.Style.default) { (alertAction) in
+            print ("Cancel without doing anything")
+        }
+        
+        
+        // put an input text field on that Alert dialog box
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Type the new item to add"
+            
+            newItemTextField = alertTextField
+            
+        }
+        
+        alert.addAction(addAction)
+        alert.addAction(cancelAction)
+        
+        
+        present (alert, animated: true)
+    }
+    
+    
+    //TODO: Declare cellForRowAtIndexPath here:"
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
 //        let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "toDoItemCell", for: indexPath)
-        
+    
         cell.textLabel?.text = itemArray[indexPath.row]
         
         return cell
